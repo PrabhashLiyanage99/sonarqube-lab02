a081cf02038bb57086d2ece26d728b50cabc81c3
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2026-01-31 22:20:11 +0530

    Fix quality issues

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,13 +1,5 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppTest {
-
-    @Test
-    void testMain() {
-        // This executes the logic in App.java and covers the lines
-        assertDoesNotThrow(() -> App.main(new String[]{}));
-    }
+public class AppTest {
+    
 }
